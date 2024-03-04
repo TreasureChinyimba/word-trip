a4d3761a2b5edd485e39c168159ef52a1647d867
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-04 16:49:40 +0200

    update

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 const meInput = document.getElementById('meInput');
 const meBtn = document.getElementById('meBtn');
 meBtn === null || meBtn === void 0 ? void 0 : meBtn.addEventListener('click', () => {
-    alert('works');
+    if (meInput.value === 'me') {
+        alert('good');
+    }
+    else {
+        if (meInput.value === '') {
+            alert('type in an answer');
+        }
+        else {
+            alert('incorrect');
+            meInput.value = '';
+        }
+    }
 });
--- script.ts
+++ script.ts
@@ -1,6 +1,18 @@
-const meInput = document.getElementById('meInput');
+const meInput = document.getElementById('meInput') as HTMLInputElement;
 const meBtn = document.getElementById('meBtn');
 
 meBtn?.addEventListener('click', ()=>{
-    alert('works')
+    if (meInput.value === 'me') {
+       alert('good')
+    } else {
+
+        if (meInput.value === '') {
+            alert('type in an answer')
+        }else {
+        alert('incorrect')
+        meInput.value = '';
+        }
+    }
+
+    
 })
